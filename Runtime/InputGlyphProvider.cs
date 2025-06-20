2c9fcfd8602c99b57c4dc13200fe95a9a7113748
Anubys <bezdigaalin@gmail.com>
2025-06-20 09:57:04 +0300

    Exclude dependency with input System

--- Runtime/InputGlyphProvider.cs
+++ Runtime/InputGlyphProvider.cs
@@ -4,10 +4,20 @@ namespace HatchStudio.Localization
 {
     public class InputGlyphProvider : ILocalizationGlyphProvider
     {
+        Func<string,string> GlyphGetter;
+        
+        public InputGlyphProvider(ActionGlyphGetter glyphGetter)
+        {
+            GlyphGetter = glyphGetter ?? throw new ArgumentNullException(nameof(glyphGetter), "Glyph getter function cannot be null.");
+        }
         public string GetGlyphForAction(string actionName)
         {
-            var bindingPath = InputManager.GetBindingPath(actionName);
-            return bindingPath?.inputGlyph.GlyphPath;
+            if (string.IsNullOrWhiteSpace(actionName))
+                throw new ArgumentException("Action name cannot be null or empty.", nameof(actionName));
+            //var bindingPath = InputManager.GetBindingPath(actionName);
+            return GlyphGetter.Invoke(actionName); //bindingPath?.inputGlyph.GlyphPath;
         }
     }
+    
+    public delegate string ActionGlyphGetter(string actionName);
 }
